8dda5d0309e0de63c5dd10d0ce0467c8ed5d3272
IFTE-13 <ifte.phoenix@gmail.com>
2025-04-18 02:04:50 +0600

    feat: new university logo

--- app/(guest)/_components/Footer/Footer.tsx
+++ app/(guest)/_components/Footer/Footer.tsx
@@ -3,6 +3,8 @@ import FooterTag from './FooterTag'
 import Link from 'next/link'
 import FooterLinks from './FooterLinks'
 import FooterSocialLinks from './FooterSocialLinks'
+import Image from 'next/image'
+import logo from '@/public/ulogo.png'
 
 const Footer = () => {
   return (
@@ -10,7 +12,7 @@ const Footer = () => {
         <footer className="relative w-full pt-4">
             <div className="w-full px-8 mx-auto max-w-screen">
                 <div className="grid justify-between border-t grid-cols-1 gap-4 md:grid-cols-1 lg:grid-cols-2">
-                    <h5 className="mb-6 text-xl font-semibold text-slate-800">Logo</h5>
+                    <Image src={logo} alt="Logo" width={400} height={400} className="mx-auto" />
                     <FooterLinks />
                 </div>
                 <div className="flex flex-col items-center justify-center w-full py-4 mt-12 border-t border-slate-200 md:flex-row md:justify-between">
